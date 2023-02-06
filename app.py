3cb554d862af9879926443212c54c6f8c8473697
Ashwini <076bct012.ashwini@pcampus.edu.np>
2023-02-06 22:42:34 +0545

    initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
+
+app = Flask(__name__)
+
+app.config["JWT_SECRET_KEY"] = "overlysecret"  # Change this!
+jwt = JWTManager(app)
+
+
+@app.route("/")
+def home():
+    return "you are in flask homepage"
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
