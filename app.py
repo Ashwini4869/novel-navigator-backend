069733b4eaae379f3a2069cc9d7aadc5d8c47523
Ashwini <076bct012.ashwini@pcampus.edu.np>
2023-02-09 19:59:25 +0545

    created db with register

--- app.py
+++ app.py
@@ -6,11 +6,27 @@ from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///users.db"
 cors = CORS(app)
 app.config["JWT_SECRET_KEY"] = "overlysecret"  # Change this!
 jwt = JWTManager(app)
+# Initializing database
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"{self.username}"
+
+
+app.app_context().push()
 
 
 @app.route("/")
@@ -25,6 +41,20 @@ def welcome():
     }
 
     return jsonify(d)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+
+    if username is None or password is None:
+        return {"error": "missing parameters. Failed to register"}, 400
+
+    user = User(username=username, password=password)
+    db.session.add(user)
+    db.session.commit()
+    return {"message": "User created successfully."}, 201
 
 
 @app.route("/login", methods=["POST"])
