337b7a3cfb8badc450335c17d4599450d1e532a8
Ashwini <076bct012.ashwini@pcampus.edu.np>
2023-03-02 21:50:37 +0545

    implemented recommendation

--- app.py
+++ app.py
@@ -29,6 +29,8 @@ bcrypt = Bcrypt(app)
 book_title_df = pd.read_json("./books.json")
 vectorizer = TfidfVectorizer()
 tfidf = vectorizer.fit_transform(book_title_df["mod_title"])
+# reading files
+books_titles = pd.read_json("books_titles_with_ratings.json")
 
 
 class User(db.Model):
@@ -134,7 +136,50 @@ def get_recommendations():
     book_list = []
     for item in object_list:
         book_list.append(item[0])
-    return jsonify(book_list), 201
+
+    def add_zero(item):
+        return ("0" + str(item))
+
+    liked_books = list(map(add_zero, book_list))
+    overlap_users = set()
+
+    with open("Ratings.csv", 'r') as f:
+        for line in f:
+            try:
+                user_id, book_id, rating = line.strip().split(",")
+                # do something with user_id, book_id, and rating
+            except ValueError:
+                # skip the line if it doesn't contain three comma-separated values
+                continue
+
+            if user_id in overlap_users:
+                continue
+
+            if book_id in liked_books and int(rating) >= 4:
+                overlap_users.add(user_id)
+    rec_lines = []
+    with open("Ratings.csv", 'r') as f:
+        for line in f:
+            try:
+                user_id, book_id, rating = line.strip().split(",")
+            except ValueError:
+                continue
+
+            if user_id in overlap_users:
+                rec_lines.append([user_id, book_id, rating])
+        # converting to df
+    recs = pd.DataFrame(rec_lines, columns=["user_id", "ISBN", "rating"])
+    recs["ISBN"] = recs["ISBN"].astype(str)
+    top_recs = recs["ISBN"].value_counts().head(5)
+    # books that are in top recs
+    books_titles[books_titles["ISBN"].isin(top_recs)]
+    all_recs = recs["ISBN"].value_counts()
+    all_recs = all_recs.to_frame().reset_index()
+    all_recs.columns = ["ISBN", "book_count"]
+    all_recs = all_recs.merge(books_titles, how="inner", on="ISBN")
+    all_recs["score"] = all_recs["book_count"] * \
+        (all_recs["book_count"] / all_recs["Book-Rating"])
+    return (all_recs.sort_values("score", ascending=False).drop_duplicates('Book-Title').head(10)).to_json(orient="records")
 
 
 if __name__ == '__main__':
